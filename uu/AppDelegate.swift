f5fd6ea337491dc2fb5c78344c24de1126d8ce05
Maxim Keegan <maxim@keegan.ru>
2023-06-26 21:42:28 +0500

    volume

--- uu/AppDelegate.swift
+++ uu/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- uu/ViewController.swift
+++ uu/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import PhotosUI
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.presentCameraController()
+        self.listenVolumeButton()
+    }
 
+    var outputVolumeObserve: NSKeyValueObservation?
+    let audioSession = AVAudioSession.sharedInstance()
+    
+    
+    
+
+    func listenVolumeButton() {
+        do {
+            try audioSession.setActive(true)
+        } catch {}
+
+        outputVolumeObserve = audioSession.observe(\.outputVolume) { (audioSession, changes) in
+            print("\(changes)")
+            MPVolumeView.setVolume(0.5)
+        }
+    }
+    
+    func presentCameraController() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = false
+        vc.delegate = self
+//        present(vc, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        // print out the image size as a test
+        print(image.size)
+        presentCameraController()
+    }
 }
 
+extension MPVolumeView {
+  static func setVolume(_ volume: Float) {
+    let volumeView = MPVolumeView()
+    let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider
+
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+      slider?.value = volume
+    }
+  }
+}
